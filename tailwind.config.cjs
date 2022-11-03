1eb4e0e8bed2fec008f72a13518592d62f513833
Pew <113262098+pewhistory@users.noreply.github.com>
2022-11-03 11:06:16 +0100

    configured tailwindcss with postcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import("tailwindcss").Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.tsx",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
